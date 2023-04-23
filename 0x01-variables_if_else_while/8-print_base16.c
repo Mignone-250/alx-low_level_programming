64365d4530de9dcb905f1fa31aa0ca4b6b195a1a
Mignone-250 <mignone.uz@gmail.com>
2023-04-23 08:04:16 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints numbers of base 16
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+	int al;
+
+	for (num = 0; num < 10 ; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
+
